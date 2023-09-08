bb6671f3f8ec4836ac2e22f58865df3fb4b76fd8
mahmoud2000yas <117920378+mahmoud2000yas@users.noreply.github.com>
2023-09-08 10:50:28 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,48 @@
+document.getElementById("htmlCode").value="<div>\n\n</div>";
+document.getElementById("cssCode").value="<style>\n\n</style>";
+document.getElementById("jsCode").value="<script>\n\n</script>";
+function showPreview(){
+    let htmlCode = document.getElementById("htmlCode").value;
+    var cssCode = ""+document.getElementById("cssCode").value+"";
+    var jsCode = ""+document.getElementById("jsCode").value+"";
+    let frame = document.getElementById("preview-window").contentWindow.document;
+    frame.open();
+    frame.write(htmlCode+cssCode+jsCode);
+    frame.close();
+    localStorage.setItem("html" ,htmlCode)
+    localStorage.setItem("css" ,cssCode)
+    localStorage.setItem("jsCode" ,jsCode)
+}
+
+if(localStorage.length>0){
+    htmlCode.value = localStorage.getItem('html')
+    cssCode.value = localStorage.getItem('css')
+    jsCode.value = localStorage.getItem('jsCode')
+}
+
+function show(x){
+    document.getElementById("html").style.display="none";
+    document.getElementById("css").style.display="none";
+    document.getElementById("js").style.display="none";
+    document.getElementById("result").style.display="none";
+    document.getElementById(x).style.display="block";
+}
+
+
+
+
+function show_all(){
+    if(window.innerWidth>=992)
+    {
+        document.getElementById("html").style.display="block";
+        document.getElementById("css").style.display="block";
+        document.getElementById("js").style.display="block";
+        document.getElementById("result").style.display="block";
+    }
+    if(window.innerWidth<992 && document.getElementById("html").style.display=="block")
+    {
+        document.getElementById("css").style.display="none";
+        document.getElementById("js").style.display="none";
+        document.getElementById("result").style.display="none";
+    }
+    }
